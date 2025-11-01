3e0e48aed7c6d45f7cf16aa97bcac84781beeddd
Rajesh Rajgor <rajeshrajgor025@gmail.com>
2025-11-01 21:06:33 +0530

    Updated code

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 IMAP_HOST = 'imap.gmail.com'
-IMAP_USER = 'rajeshrajgor00@gmail.com'
-IMAP_PASSWORD = 'vlovmeispegqqznj'   
+IMAP_USER = 'yourmail@gmail.com'
+IMAP_PASSWORD = 'yourapppassword'   
 MAIL_FOLDER = 'INBOX'
 SEARCH_CRITERIA = 'ALL'
 RESUMES_DIR = 'resumes'
